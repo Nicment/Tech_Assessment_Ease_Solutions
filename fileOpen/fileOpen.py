80902218a5ae20e7889c815a99da477068027edf
Nicolas <nicment97@gmail.com>
2021-12-09 19:45:03 -0500

    Add initial class to solve the challenge

--- fileOpen/fileOpen.py
+++ fileOpen/fileOpen.py
@@ -1,1 +1,20 @@
 #todo: load file, data verification, pre-proces data, return data
+
+class fileOpen():
+    def __init__(self, filePath:str="skirsesort.kitzbuehel/4x4.txt"):
+        self.filePath = filePath
+        self.readFile()
+        self.checkData()
+        self.preProcess()
+        return self.exportData()
+        
+    def readFile(self):
+        pass
+    def checkData(self):
+        pass
+    def preProcess(self):
+        pass
+    def exportData(self):
+        pass
+    
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from fileOpen import fileOpen
+from challengeSolution import challengeSolution
+
+class main():
+    def __init__(self):
+        self.fileOpen = fileOpen()
