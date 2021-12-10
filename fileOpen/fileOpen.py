1d85e3b2d08ba12f88f07418921698266d4eb365
Nicolas <nicment97@gmail.com>
2021-12-10 06:48:33 -0500

    The function to read the file is added

--- fileOpen/fileOpen.py
+++ fileOpen/fileOpen.py
@@ -1,20 +1,33 @@
+import numpy as np
 #todo: load file, data verification, pre-proces data, return data
 
+# readFile help to read file and return list with the information
 class fileOpen():
     def __init__(self, filePath:str="skirsesort.kitzbuehel/4x4.txt"):
-        self.filePath = filePath
-        self.readFile()
+        data = self.readFile(filePath)      
+        print (data) 
         self.checkData()
         self.preProcess()
         return self.exportData()
         
-    def readFile(self):
-        pass
+    def readFile(self, filePath): 
+        a_file = open(filePath)
+        file_contents = a_file.read()
+        contents_split = file_contents.splitlines()
+        contents_split = [contents_split[i].split(" ") for i in range(len(contents_split))]
+        return [list(map(int,contents_split[i])) for i in range(len(contents_split))]
+    
     def checkData(self):
         pass
+    
     def preProcess(self):
         pass
+    
     def exportData(self):
         pass
-    
-        
+
+def main():
+    fileOpen()   
+
+if __name__ == '__main__':
+    main()    
