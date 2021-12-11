90fb4a9d99bf8d7eae89b04afba54dcfe518368a
Nicolas <nicment97@gmail.com>
2021-12-11 06:57:38 -0500

    error correction and code optimization

--- challengeSolution/challengeSolution.py
+++ challengeSolution/challengeSolution.py
@@ -41,305 +41,135 @@ class vertex():# an object is created that will contain the data of each vertex
     def setBestWay(self, bestWay):
         self.bestWay += bestWay
     
+    def bestWaySelected(self, northWay:list=[],southWay:list =[],eastWay:list =[],westWay:list=[]):
+        
+        northWaySteps = len(northWay)
+        southWaySteps = len(southWay)
+        eastWaySteps = len(eastWay)
+        westWaySteps = len(westWay)
+        
+        if northWaySteps < 0:
+            northWayHeight = northWay[0] - northWay[northWaySteps-1] + 1
+        else:
+            northWayHeight = 0
+        if southWaySteps < 0:
+            southWayHeight = southWay[0] - southWay[southWaySteps-1] + 1
+        else:
+            southWayHeight = 0
+        if westWaySteps < 0:
+            westWayHeight = southWay[0] - southWay[eastWaySteps-1] + 1
+        else:
+            westWayHeight = 0
+        if eastWaySteps < 0:
+            eastWayHeight = westWay[0] - westWay[westWaySteps-1] + 1
+        else:
+            eastWayHeight = 0
+        
+        maxSteps = max(northWaySteps,max(southWaySteps,max(eastWaySteps,westWaySteps)))
+        maxHeight = max(northWayHeight,max(southWayHeight,max(eastWayHeight,westWayHeight)))
+        if northWaySteps == maxSteps and northWayHeight == maxHeight:
+            bestWay =  northWay
+        elif southWaySteps == maxSteps and southWayHeight == maxHeight:
+            bestWay = southWay
+        elif eastWaySteps == maxSteps and eastWayHeight == maxHeight:
+            bestWay = eastWay
+        elif westWaySteps == maxSteps and westWayHeight == maxHeight:
+            bestWay = westWay  
+        
+        return bestWay
+        
+    
     def findBestWay(self):
         # Upper left corner
-        if self.i == 0 and self.j == 0:
-            vertex = self.matrix[self.i][self.j]                  
+        if self.i == 0 and self.j == 0:                
             south = self.matrix[self.i+1][self.j]
             east = self.matrix[self.i][self.j+1]
-            
-            bestWay0 = []
-            bestWay1 = []
-
-            if vertex.getValue() > south.getValue():
-                if south.visited == False:
-                    bestWay0 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + south.findBestWay()         
-            elif east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay1 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + east.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-                
-            vertex.setBestWay(max(bestWay0, bestWay1))
-            vertex.setVisited = True
-            print (max(bestWay0, bestWay1))
-            return max(bestWay0, bestWay1)
-                
-                
-        #Upper middle vertex
-        if self.i == 0 and self.j > 0 and self.j < self.size[1]-1:
-            vertex = self.matrix[self.i][self.j]
+            north = self
+            west = self
+        elif self.i == 0 and self.j > 0 and self.j < self.size[1]-1:
+            north = self
             south = self.matrix[self.i+1][self.j]
             east = self.matrix[self.i][self.j+1]
             west = self.matrix[self.i][self.j-1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            bestWay2 = []
-            
-            #Revisa si hay mas posibles caminos hacia el sur
-            if south.getValue() < vertex.getValue():
-                if south.visited == False:
-                    bestWay0 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + south.bestWay
-            #Revisa si hay mas posibles caminos hacia el este   
-            elif east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay1 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + east.bestWay
-            #Revisa si hay mas posibles caminos hacia el oeste 
-            elif west.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay2 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay2 = [vertex.getValue()] + east.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-            
-            #Evalua los caminos resultantes y se queda con el mejor    
-            vertex.setBestWay(max(bestWay0, max(bestWay1, bestWay2)))
-            vertex.visited = True
-            return max(bestWay0, max(bestWay1, bestWay2))
-            
-        #Upper right corner
-        if self.j == self.size[1]-1 and self.i == 0:
-            vertex = self.matrix[self.i][self.j]
+        elif self.j == self.size[1]-1 and self.i == 0:
             south = self.matrix[self.i+1][self.j]
             west = self.matrix[self.i][self.j-1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            
-            if south.getValue() < vertex.getValue():
-                if south.visited == False:
-                    bestWay0 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + south.bestWay                
-            elif west.getValue() < vertex.getValue():
-                if west.visited == False:
-                    bestWay1 = [vertex.getValue()] + west.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + west.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-                
-            vertex.setBestWay(max(bestWay0, bestWay1))
-            vertex.visited = True
-            return max(bestWay0, bestWay1)
-            
-        #Left middle vertex
-        if self.j == 0 and self.i > 0 and self.i < self.size[0]-1:
-            vertex = self.matrix[self.i][self.j]
+            north = self
+            east = self
+        elif self.j == 0 and self.i > 0 and self.i < self.size[0]-1:
             north = self.matrix[self.i-1][self.j]
             south = self.matrix[self.i+1][self.j]
             east = self.matrix[self.i][self.j+1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            bestWay2 = []
-            
-            #Revisa si hay mas posibles caminos hacia el norte
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + east.bestWay        
-            #Revisa si hay mas posibles caminos hacia el sur
-            elif south.getValue() < vertex.getValue():
-                if south.visited == False:
-                    bestWay1 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + south.bestWay
-            #Revisa si hay mas posibles caminos hacia el este   
-            elif east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay2 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay2 = [vertex.getValue()] + east.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-            #Revisa si hay mas posibles caminos hacia el oeste 
-           
-            #Evalua los caminos resultantes y se queda con el mejor    
-            vertex.bestWay = max(bestWay0, max(bestWay1, bestWay2))
-            vertex.visited = True
-            return max(bestWay0, max(bestWay1, bestWay2))
-        
-        #Middle vertex
-        if self.i > 0 and self.i < self.size[0]-1 and self.j > 0 and self.j < self.size[1]-1:
-            vertex = self.matrix[self.i][self.j]
+            west = self
+        elif self.i > 0 and self.i < self.size[0]-1 and self.j > 0 and self.j < self.size[1]-1:
             north = self.matrix[self.i-1][self.j]
             south = self.matrix[self.i+1][self.j]
             east = self.matrix[self.i][self.j+1]
             west = self.matrix[self.i][self.j-1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            bestWay2 = []
-            bestWay3 = []
-            
-            #Revisa si hay mas posibles caminos hacia el norte
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + north.bestWay
-            #Revisa si hay mas posibles caminos hacia el sur
-            elif south.getValue() < vertex.getValue():
-                if south.visited == False:
-                    bestWay1 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + south.bestWay
-            #Revisa si hay mas posibles caminos hacia el este   
-            elif east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay2 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay2 = [vertex.getValue()] + east.bestWay
-            #Revisa si hay mas posibles caminos hacia el oeste   
-            elif west.getValue() < vertex.getValue():
-                if west.visited == False:
-                    bestWay3 = [vertex.getValue()] + west.findBestWay()
-                else:
-                    bestWay3 = [vertex.getValue()] + west.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-                
-            #Evalua los caminos resultantes y se queda con el mejor    
-            vertex.setBestWay(max(bestWay0, max(bestWay1, max(bestWay2,bestWay3))))
-            vertex.visited = True
-            return max(bestWay0, max(bestWay1, max(bestWay2,bestWay3)))
-            
         #Right vertex
-        if self.j == self.size[1]-1 and self.i > 0 and self.i < self.size[0]-1:
-            vertex = self.matrix[self.i][self.j]
+        elif self.j == self.size[1]-1 and self.i > 0 and self.i < self.size[0]-1:
             north = self.matrix[self.i-1][self.j]
             south = self.matrix[self.i+1][self.j]
             west = self.matrix[self.i][self.j-1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            bestWay2 = []
-            
-            #Revisa si hay mas posibles caminos hacia el norte
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + north.bestWay
-            #Revisa si hay mas posibles caminos hacia el sur
-            elif south.getValue() < vertex.getValue():
-                if south.visited == False:
-                    bestWay1 = [vertex.getValue()] + south.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + south.bestWay 
-            #Revisa si hay mas posibles caminos hacia el oeste   
-            elif west.getValue() < vertex.getValue():
-                if west.visited == False:
-                    bestWay2 = [vertex.getValue()] + west.findBestWay()
-                else:
-                    bestWay2 = [vertex.getValue()] + west.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-            #Evalua los caminos resultantes y se queda con el mejor    
-            vertex.setBestWay(max(bestWay0, max(bestWay1, bestWay2)))
-            vertex.visited = True
-            return max(bestWay0, max(bestWay1, bestWay2))
-        
-        #Lower left corner
-        if self.i == self.size[1]-1 and self.j == 0:
-            vertex = self.matrix[self.i][self.j]
+            east = self
+        elif self.i == self.size[1]-1 and self.j == 0:
             north = self.matrix[self.i-1][self.j]
             east = self.matrix[self.i][self.j+1]
-            
-            bestWay0 = []
-            bestWay1 = []
-            
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + north.bestWay
-            if east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay1 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + east.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-                
-            vertex.setBestWay(max(bestWay0, bestWay1))
-            vertex.visited = True
-            return max(bestWay0, bestWay1)
-        #Lower middle vertex
-        if self.j > 0 and self.j < self.size[1]-1 and self.i == self.size[0]-1:
-            vertex = self.matrix[self.i][self.j]
+            west = self
+            south = self
+        elif self.j > 0 and self.j < self.size[1]-1 and self.i == self.size[0]-1:
             north = self.matrix[self.i-1][self.j]
             east = self.matrix[self.i][self.j+1]
             west = self.matrix[self.i][self.j-1]  
-            
-            bestWay0 = []
-            bestWay1 = []
-            bestWay2 = []
-            
-            #Revisa si hay mas posibles caminos hacia el sur
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + north.bestWay
-            #Revisa si hay mas posibles caminos hacia el este   
-            elif east.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay1 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + east.bestWay
-            #Revisa si hay mas posibles caminos hacia el oeste 
-            elif west.getValue() < vertex.getValue():
-                if east.visited == False:
-                    bestWay2 = [vertex.getValue()] + east.findBestWay()
-                else:
-                    bestWay2 = [vertex.getValue()] + east.bestWay
-            else:
-                bestWay0 += [vertex.getValue()]
-            
-            #Evalua los caminos resultantes y se queda con el mejor    
-            vertex.setBestWay(max(bestWay0, max(bestWay1, bestWay2)))
-            vertex.visited = True
-            return max(bestWay0, max(bestWay1, bestWay2))
-        #Lower right corner
-        if self.j == self.size[1]-1 and self.i == self.size[0]-1:
-            vertex = self.matrix[self.i][self.j]
+            south = self
+        elif self.j == self.size[1]-1 and self.i == self.size[0]-1:
             north = self.matrix[self.i-1][self.j]
             west = self.matrix[self.i][self.j-1]
+            east = self
+            south = self
             
-            bestWay0 = []
-            bestWay1 = []
-            
-            if north.getValue() < vertex.getValue():
-                if north.visited == False:
-                    bestWay0 = [vertex.getValue()] + north.findBestWay()
-                else:
-                    bestWay0 = [vertex.getValue()] + north.bestWay
-            elif west.getValue() < vertex.getValue():
-                if west.visited == False:
-                    bestWay1 = [vertex.getValue()] + west.findBestWay()
-                else:
-                    bestWay1 = [vertex.getValue()] + west.bestWay
+        print(north.getValue(), south.getValue(), east.getValue(),west.getValue())
+        
+        northWay = []
+        southWay = []
+        eastWay = []
+        westWay = []
+        
+        #Revisa si hay mas posibles caminos hacia el norte
+        if north.getValue() < self.getValue():
+            if north.visited == False:
+                northWay = [self.getValue()] + north.findBestWay()
             else:
-                bestWay0 += [vertex.getValue()]
-                
-            vertex.setBestWay(max(bestWay0, bestWay1))
-            vertex.visited = True
-            return max(bestWay0, bestWay1)
-        #TODO: revisar vecinos, si es menor y ya es visitado, toma el mejor camino
-        #sino, encuentra el mejor camino
-        #todo: 2. Evalua el mejor camino
-        #todo: 3. Retorna el mejor camino  
-        pass 
+                northWay = [self.getValue()] + north.bestWay
+        #Revisa si hay mas posibles caminos hacia el sur
+        elif south.getValue() < self.getValue():
+            if south.visited == False:
+                southWay = [self.getValue()] + south.findBestWay()
+            else:
+                southWay = [self.getValue()] + south.bestWay
+        #Revisa si hay mas posibles caminos hacia el este   
+        elif east.getValue() < self.getValue():
+            if east.visited == False:
+                eastWay = [self.getValue()] + east.findBestWay()
+            else:
+                eastWay = [self.getValue()] + east.bestWay
+        #Revisa si hay mas posibles caminos hacia el oeste   
+        elif west.getValue() < self.getValue():
+            if west.visited == False:
+                westWay = [self.getValue()] + west.findBestWay()
+            else:
+                westWay = [self.getValue()] + west.bestWay
+        else:
+            self.bestWay = [self.getValue()]
+            northWay = [self.getValue()] 
+            
+        #Evalua los caminos resultantes y se queda con el mejor    
+        self.setBestWay(self.bestWaySelected(northWay, southWay, eastWay, westWay))
+        self.visited = True
+        return self.bestWaySelected(northWay, southWay, eastWay, westWay)
+    
+    #TODO: revisar vecinos, si es menor y ya es visitado, toma el mejor camino
+    #sino, encuentra el mejor camino
+    #todo: 2. Evalua el mejor camino
+    #todo: 3. Retorna el mejor camino  
+    pass 
